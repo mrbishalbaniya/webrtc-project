a168049f887db4dbc6d938159bec045798cab23a
Bishal Baniya <35622189+mrbishalbaniya@users.noreply.github.com>
2025-03-06 13:32:01 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Signaling server running on https://wrcvc.netlify.app/');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });

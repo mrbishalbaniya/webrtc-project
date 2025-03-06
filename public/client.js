37cc0cce86149193cf13900ab0985329505bb546
mrbishalbaniya <mr.bishal.baniya@gmail.com>
2025-03-06 13:01:20 +0545

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,64 @@
+const socket = io('http://localhost:3000');
+
+// Get video elements
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
+// Create a peer connection
+const peerConnection = new RTCPeerConnection({
+    iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' } // Free STUN server
+    ]
+});
+
+// Set up media streams
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then((stream) => {
+        localVideo.srcObject = stream;
+        stream.getTracks().forEach((track) => {
+            peerConnection.addTrack(track, stream);
+        });
+    })
+    .catch((error) => {
+        console.error('Error accessing media devices:', error);
+    });
+
+// Handle remote stream
+peerConnection.ontrack = (event) => {
+    remoteVideo.srcObject = event.streams[0];
+};
+
+// Handle ICE candidates
+peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+        socket.emit('signal', { type: 'candidate', candidate: event.candidate });
+    }
+};
+
+// Handle signaling messages
+socket.on('signal', (data) => {
+    if (data.type === 'offer') {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
+            .then(() => peerConnection.createAnswer())
+            .then((answer) => peerConnection.setLocalDescription(answer))
+            .then(() => {
+                socket.emit('signal', { type: 'answer', answer: peerConnection.localDescription });
+            });
+    } else if (data.type === 'answer') {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+    } else if (data.type === 'candidate') {
+        peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+    }
+});
+
+// Start the call
+function startCall() {
+    peerConnection.createOffer()
+        .then((offer) => peerConnection.setLocalDescription(offer))
+        .then(() => {
+            socket.emit('signal', { type: 'offer', offer: peerConnection.localDescription });
+        });
+}
+
+// Start the call when the page loads
+startCall();
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the "public" folder
+app.use(express.static('../public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Relay signaling messages between peers
+    socket.on('signal', (data) => {
+        socket.broadcast.emit('signal', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Signaling server running on http://localhost:3000');
+});
